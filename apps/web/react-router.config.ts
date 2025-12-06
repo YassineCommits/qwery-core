e2b2650ebb31b1b4c29ad44a3d7ebd3bc81bca11
Hani CHALOUATI <hani.chalouati@gmail.com>
2025-12-06 16:00:06 +0100

    ci: add vercel preset

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from '@react-router/dev/config';
 
-//import { vercelPreset } from '@vercel/react-router/vite';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   ssr: true,
-  presets: [
-    // vercelPreset()
-  ],
+  presets: [vercelPreset()],
 } satisfies Config;
